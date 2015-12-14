6ecc6c503aeca5376a64a8ef3e608b9140959d9a
tpiotg36 <leejiaen1212@gmail.com>
2015-12-14 20:36:33 +0800

    Project

---
+++ relayoff.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python  
+import RPi.GPIO as GPIO  
+from time import time  
+  
+#GPIO.setmode(GPIO.BOARD)  
+#GPIO.setwarnings(False)
+#number = 40
+#GPIO.setup(number,GPIO.OUT)  
+#GPIO.output(number,True)
+
+
+GPIO.setmode(GPIO.BCM)  
+number = 21  
+GPIO.setup(number, GPIO.OUT)  
+GPIO.output(number, GPIO.LOW) 
